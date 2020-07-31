ae3d1a464f0683e7d2174ea2a37299ceccd12e86
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-07-31 16:37:02 -0300

    app clone

---
+++ lib/src/pages/login/welcome_screen.dart
@@ -0,0 +1,56 @@
+import 'package:duolingo/src/pages/login/login_app.dart';
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  _body(context){
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.only(top: 125.0, left: 16.0, right: 16.0, bottom: 16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/logo-with-duo.png', width: 270,),
+            SizedBox(height: 22,),
+            SizedBox(
+              width: 280,
+              child: Text('Aprenda idiomas de graça. Agora e sempre.', style:
+              TextStyle(fontSize: 26, color: Colors.grey.shade400,),),
+            ),
+            SizedBox(height: 145,),
+            _button('COMEÇAR AGORA', color: Colors.lightGreen, onPressed: (){}, colorText: Colors.white),
+            SizedBox(height: 12,),
+            _button('JÁ TENHO UMA CONTA', color: Colors.white, onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginApp()));
+                  }, colorText: Colors.green),
+                ],
+            ),
+      ),
+    );
+  }
+  _button(String text, {Color color, Color colorText, Function onPressed}){
+    return Container(
+      width: 350,
+      height: 60,
+      decoration: BoxDecoration(boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.5),
+          spreadRadius: 2,
+          blurRadius: 12,
+          offset: Offset(0, 2),
+        ),
+      ]),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0),),
+        color: color,
+        child: Text(text, style: TextStyle(color: colorText, fontSize: 23, fontWeight: FontWeight.bold),),
+        onPressed: onPressed,
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _body(context),
+    );
+  }
+}
---
+++ lib/src/pages/main_screen/home_screen.dart
@@ -0,0 +1,29 @@
+//import 'package:flutter/material.dart';
+//
+//class HomeScreen extends StatefulWidget {
+//  @override
+//  _HomeScreenState createState() => _HomeScreenState();
+//}
+//
+//class _HomeScreenState extends State<HomeScreen> {
+//  _body(){
+//    return ListView(
+//      children: <Widget>[
+//
+//      ],
+//    );
+//  }
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        backgroundColor: Colors.white,
+//        actions: <Widget>[
+//
+//        ],
+//      ),
+//
+//      body: _body(),
+//    );
+//  }
+//}
