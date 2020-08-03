8fdc83701fa3dd30ae6225fd3ee49bb4092f3e72
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-03 15:02:57 -0300

    Refatoração do método appBar

---
+++ lib/src/pages/main_screen/appbar_home_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class AppBarHomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+          leading: IconButton(
+            padding: EdgeInsets.only(left: 16.0),
+            icon: Image.asset('assets/images/appBar/flag.png'),
+            onPressed: () {},
+          ),
+          elevation: 5,
+          actions: <Widget>[
+            Row(
+              children: [
+                IconButton(
+                  icon: Image.asset(
+                    'assets/images/appBar/crown_stroke.png',
+                    height: 29,
+                  ),
+                  onPressed: () {},
+                ),
+                Text(
+                  '67',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.amber,
+                      fontSize: 17),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                IconButton(
+                  icon: Image.asset(
+                    'assets/images/appBar/navbar_fire.png',
+                    height: 29,
+                  ),
+                  onPressed: () {},
+                ),
+                Text('50',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 17)),
+                SizedBox(
+                  width: 40,
+                ),
+                IconButton(
+                  padding: EdgeInsets.only(left: 16.0),
+                  icon: Image.asset(
+                    'assets/images/appBar/navbar_diamont.png',
+                    height: 29,
+                  ),
+                  onPressed: () {},
+                ),
+                Text(
+                  '130',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red,
+                      fontSize: 17),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+          ],
+    );
+  }
+}
--- lib/src/pages/main_screen/home_screen.dart
+++ lib/src/pages/main_screen/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:duolingo/src/pages/main_screen/appbar_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'circle_avatar.dart';
 
@@ -96,74 +97,12 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    var appBar = AppBarHomeScreen();
+    
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(45.0),
-        child: AppBar(
-          backgroundColor: Colors.white,
-          leading: IconButton(
-            padding: EdgeInsets.only(left: 16.0),
-            icon: Image.asset('assets/images/appBar/flag.png'),
-            onPressed: () {},
-          ),
-          elevation: 5,
-          actions: <Widget>[
-            Row(
-              children: [
-                IconButton(
-                  icon: Image.asset(
-                    'assets/images/appBar/crown_stroke.png',
-                    height: 29,
-                  ),
-                  onPressed: () {},
-                ),
-                Text(
-                  '67',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.amber,
-                      fontSize: 17),
-                ),
-                SizedBox(
-                  width: 30,
-                ),
-                IconButton(
-                  icon: Image.asset(
-                    'assets/images/appBar/navbar_fire.png',
-                    height: 29,
-                  ),
-                  onPressed: () {},
-                ),
-                Text('50',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.grey,
-                        fontSize: 17)),
-                SizedBox(
-                  width: 40,
-                ),
-                IconButton(
-                  padding: EdgeInsets.only(left: 16.0),
-                  icon: Image.asset(
-                    'assets/images/appBar/navbar_diamont.png',
-                    height: 29,
-                  ),
-                  onPressed: () {},
-                ),
-                Text(
-                  '130',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                      fontSize: 17),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-              ],
-            ),
-          ],
-        ),
+        child: appBar,
       ),
       body: _body(),
       floatingActionButton: FloatingActionButton(
