05122c6ff4095f37b6022d6d49449240e7c1ddf7
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-03 14:50:57 -0300

    bloqueio de rotação de tela

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:duolingo/src/pages/login/welcome_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Duolingo',
