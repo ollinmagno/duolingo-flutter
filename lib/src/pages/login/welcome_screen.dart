6fc8af22778012643d779f65a8268d0dd5ad1a00
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-03 16:48:43 -0300

    layout responsivo com LayoutBuilder

--- lib/src/pages/login/welcome_screen.dart
+++ lib/src/pages/login/welcome_screen.dart
@@ -2,32 +2,7 @@ import 'package:duolingo/src/pages/login/login_app.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
-  _body(context){
-    return Center(
-      child: Container(
-        padding: const EdgeInsets.only(top: 125.0, left: 16.0, right: 16.0, bottom: 16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/logo-with-duo.png', width: 270,),
-            SizedBox(height: 22,),
-            SizedBox(
-              width: 280,
-              child: Text('Aprenda idiomas de graça. Agora e sempre.', style:
-              TextStyle(fontSize: 26, color: Colors.grey.shade400,),),
-            ),
-            Flexible(child: FractionallySizedBox(heightFactor: 0.75,),),
-            _button('COMEÇAR AGORA', color: Colors.lightGreen, onPressed: (){}, colorText: Colors.white),
-            SizedBox(height: 12,),
-            _button('JÁ TENHO UMA CONTA', color: Colors.white, onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context) => LoginApp()));
-                  }, colorText: Colors.green),
-                ],
-            ),
-      ),
-    );
-  }
-  _button(String text, {Color color, Color colorText, Function onPressed}){
+  _button(String text, {Color color, Color colorText, Function onPressed}) {
     return Container(
       width: 350,
       height: 60,
@@ -40,17 +15,73 @@ class WelcomeScreen extends StatelessWidget {
         ),
       ]),
       child: RaisedButton(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.0),),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0),
+        ),
         color: color,
-        child: Text(text, style: TextStyle(color: colorText, fontSize: 23, fontWeight: FontWeight.bold),),
+        child: Text(
+          text,
+          style: TextStyle(
+              color: colorText, fontSize: 23, fontWeight: FontWeight.bold),
+        ),
         onPressed: onPressed,
       ),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _body(context),
+      body: Center(
+        child: LayoutBuilder(builder: (_, constraints) {
+          return Container(
+            color: Colors.white,
+            height: constraints.maxHeight,
+            width: constraints.maxWidth,
+            padding: const EdgeInsets.only(
+                top: 125.0, left: 16.0, right: 16.0, bottom: 16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/logo-with-duo.png',
+                  width: 270,
+                ),
+                SizedBox(
+                  height: 22,
+                ),
+                SizedBox(
+                  width: 280,
+                  child: Text(
+                    'Aprenda idiomas de graça. Agora e sempre.',
+                    style: TextStyle(
+                      fontSize: 26,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                ),
+                Flexible(
+                  child: FractionallySizedBox(
+                    heightFactor: 0.75,
+                  ),
+                ),
+                _button('COMEÇAR AGORA',
+                    color: Colors.lightGreen,
+                    onPressed: () {},
+                    colorText: Colors.white),
+                SizedBox(
+                  height: 12,
+                ),
+                _button('JÁ TENHO UMA CONTA', color: Colors.white,
+                    onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginApp()));
+                }, colorText: Colors.green),
+              ],
+            ),
+          );
+        }),
+      ),
     );
   }
 }
