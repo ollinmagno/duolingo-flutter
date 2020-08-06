06460a6bf785d5926b289b8ebc452892f08b6423
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-05 21:57:40 -0300

    add widget button

---
+++ lib/src/widgets/button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final String text;
+  final Function onPressed;
+  final Color color;
+  final Color colorText;
+  final double widget;
+  final bool showProgress;
+
+  Button(this.text,
+      {this.onPressed,
+      this.color,
+      this.colorText,
+      this.widget,
+      this.showProgress = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 46,
+      child: RaisedButton(
+        color: Colors.blue,
+        child: showProgress
+            ? Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                ),
+              )
+            : Text(
+                text,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22,
+                ),
+              ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
