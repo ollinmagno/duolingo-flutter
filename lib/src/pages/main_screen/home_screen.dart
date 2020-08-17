35062ae9b6b6df46220be9b191c06c7d3317cd86
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-17 19:29:52 -0300

    current index

--- lib/src/pages/main_screen/home_screen.dart
+++ lib/src/pages/main_screen/home_screen.dart
@@ -95,10 +95,10 @@ class _HomeScreenState extends State<HomeScreen> {
       ],
     );
   }
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    int _currentIndex = 0;
     final double _iconSize = 41;
     final double _iconSizeSelected = 53;
     var appBar = AppBarHomeScreen();
