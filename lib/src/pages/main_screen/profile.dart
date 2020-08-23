31194d2b574e4f45ff8d52e18db6036e8a27cf80
Ollin Magno <41454237+ollinmagno@users.noreply.github.com>
2020-08-23 14:32:27 -0300

    layout profile

--- lib/src/pages/main_screen/profile.dart
+++ lib/src/pages/main_screen/profile.dart
@@ -7,46 +7,123 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  _titleText(String text) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+    );
+  }
+
   @override
-  Widget build(BuildContext context) =>
-    Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: Text(
-                          "User Name",
-                          style: TextStyle(
-                              fontSize: 23, fontWeight: FontWeight.bold),
-                        ),
+  Widget build(BuildContext context) => Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _titleText("User Name"),
+                    const SizedBox(width: 100),
+                    ClipOval(
+                      child: Image.asset(
+                        "assets/images/logo-with-duo.png",
+                        height: 120,
                       ),
-                    ],
-                  ),
-                  const SizedBox(width: 90),
-                  Column(
-                    children: <Widget>[
-                      ClipOval(
-                        child: Image.asset(
-                          "assets/images/logo-with-duo.png",
-                          height: 120,
+                    ),
+                  ],
+                ),
+                Divider(color: Colors.grey.shade500),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _titleText("Sobre"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Card(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 80,
+                            width: 170,
+                            child: Text("hello"),
+                          ),
                         ),
+                        Card(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 80,
+                            width: 170,
+                            child: ListTile(
+                                leading: Icon(
+                                  Icons.cloud_circle,
+                                  color: Colors.amber,
+                                ),
+                                title: Text(
+                                  "4600",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20),
+                                )),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Card(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 80,
+                            width: 170,
+                            child: Text("hello"),
+                          ),
+                        ),
+                        Card(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            height: 80,
+                            width: 170,
+                            child: Text("hello"),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 30),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        _titleText("Amigos"),
+                        Text("ADICIONAR AMIGOS", style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold,
+                          color: Colors.lightBlue),),
+                      ],
+                    ),
+                    Card(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        height: 80,
+                        width: 170,
                       ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
 }
